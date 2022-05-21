64c04d84c7d7c5a1d66362b2bbbb9f86f87df995
Goodluck <igor.obidny@gmail.com>
2022-05-21 19:21:39 +0300

    гирлянда

---
+++ src/com/Gextracted.java
@@ -0,0 +1,14 @@
+package com;
+
+
+
+public class Gextracted {
+    public static void extracted(int[][] xz) {
+        for (int[] x : xz) {
+            for (int y : x) {
+                System.out.print(y + " ");
+            }
+            System.out.println();
+        }
+    }
+}
--- src/com/prim5.java
+++ src/com/prim5.java
@@ -2,9 +2,14 @@ package com;
 
 import java.util.Arrays;
 
+
+
+
+
 public class prim5 {
 
     public static void main(String[] args) {
+        System.out.println("HI");
        int[][] dz = {
                {45,8,69,25,1,14,65,77},
                {33,22,35,44,78,56,6,99},
@@ -12,14 +17,15 @@ public class prim5 {
                {1,3,74,66,34,23,78,98},
                {91,25,73,16,28,4,49,61}
                };
-        extracted(dz);
+       Gextracted cd = new Gextracted();
+        cd.extracted(dz);
         System.out.println();
        Arrays.sort(dz[0]);
        Arrays.sort(dz[1]);
        Arrays.sort(dz[2]);
        Arrays.sort(dz[3]);
        Arrays.sort(dz[4]);
-        extracted(dz);
+        cd.extracted(dz);
         System.out.println();
         int[][] min_max=new int[5][2];
         System.arraycopy(dz[0],0,min_max[0],0,1);
@@ -32,15 +38,15 @@ public class prim5 {
         System.arraycopy(dz[2],7,min_max[2],1,1);
         System.arraycopy(dz[3],7,min_max[3],1,1);
         System.arraycopy(dz[4],7,min_max[4],1,1);
-        extracted(min_max);
+        cd.extracted(min_max);
     }
-
+/*
     private static void extracted(int[][] dz) {
         for (int[]x: dz) {
             for (int y:x ) {
                 System.out.print(y+ " ");
             }
             System.out.println();
-        }
+        }*/
     }
-}
+
